3566ee0a45d9ea69b9df9fd86a72f125ad1e062e
Antonio <hedhogs@gmail.com>
2023-02-16 00:09:11 +0100

    fix: naming

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
-import Layout from '../../components/Layout';
-import Date from '../../components/Date';
+import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
 
